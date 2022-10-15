abe39c28af08ef659ff3120177876962057cb6ab
KatieRina <eashchukina@yandex.ru>
2022-10-15 15:09:00 +0300

    третья задача

---
+++ HW6/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int mod = Math.Abs(a) % 2;
+if (mod == 1) Console.WriteLine("Нечётное");
+if (mod == 0) Console.WriteLine("Чётное");
