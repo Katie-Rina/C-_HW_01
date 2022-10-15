75ac1377bc8860a22fd6a854f3e179cb3b5687c2
KatieRina <eashchukina@yandex.ru>
2022-10-15 13:17:06 +0300

    семинар

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-// Console.Clear();
-// Console.Write("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// int sqrt = number * number;
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number * number;
 // number *= number;
 // int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
-// Console.WriteLine("Число в квадрате: " + sqrt);
+Console.WriteLine("Число в квадрате: " + sqrt);
 
